2a76488b93bb448d47ddd4cd4f282e225e3eb431
jambilimukunda <jambilinaveen2@gmail.com>
2025-02-15 20:41:40 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import joblib
 
 # Load the models (change paths to where your models are saved)
-model_skin_cancer = tf.keras.models.load_model(r'C:\Users\navee\Downloads')
+model_skin_cancer = tf.keras.models.load_model(r'C:\Users\navee\Downloads\skin.h5')
 
 model_maternal_health = joblib.load('app/model.bin')
 
